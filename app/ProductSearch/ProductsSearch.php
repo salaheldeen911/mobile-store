4fe61f21de7f8f447115be3de7b8440ca40a1b41
salah <salah.eldeen.mail@gmail.com>
2022-11-11 20:51:56 +0200

    update namespaces

--- app/ProductSearch/ProductsSearch.php
+++ app/ProductSearch/ProductsSearch.php
@@ -3,48 +3,11 @@
 namespace App\ProductSearch;
 
 use App\Models\Product;
-use App\Http\Requests;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Illuminate\Database\Eloquent\Builder;
 
 class ProductsSearch
 {
-    // public static function apply(Request $filters, Product $product)
-    // {
-    //     $query = $product->newQuery();
-
-    //     $query = static::applyFiltersToQuery($filters, $query);
-
-    //     if ($filters->has('name')) {
-    //         $products->where('name', 'like', '%' . $filters->name . '%');
-    //     }
-    //     if ($filters->has('category')) {
-    //         $products->whereIn('category', $filters->category);
-    //     }
-    //     if ($filters->has('price')) {
-    //         foreach ($filters->price as $value) {
-    //             $products->whereBetween('price', [$value, $value * 2]);
-    //         }
-    //     }
-    //     if ($filters->has('brand')) {
-    //         $products->whereIn('brand', $filters->brand);
-    //     }
-    //     if ($filters->has('color')) {
-    //         $products->where('color', $filters->color);
-    //     }
-    //     if ($filters->has('sort') && $filters->sort !== null) {
-    //         $products = $products->orderBy('price', $filters->sort);
-    //     }
-
-    //     return $products->paginate(4);
-    // }
-
-    // public static function applyFiltersToQuery(Request $filters, $query)
-    // {
-    // }
-
-
     public static function apply(Request $filters)
     {
         $query = static::applyDecoratorsFromRequest($filters, (new Product())->newQuery());
